552b644b5ee010b640a45d847768f42ca45f42eb
Kuyik Usen Brown <kuyikbrown@gmail.com>
2023-03-18 10:59:19 -0700

    hello World project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#include <stdio.h>
+/** 
+ * main - tentry point for all our code
+ *
+ * Return: must be a number or alphabet
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
